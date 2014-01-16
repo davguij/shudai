242d42fadd1b941cf5511d10a4166b2bed9b6bdb
David Guijarro <davidguijarro@Mac-mini-de-Worten.local>
2014-01-16 22:22:01 +0100

    Revert "Revert "Diferencia entre datos de tabla y de campo de txt""
    This reverts commit 0d52969471a5ba7a42f98f946579cea697c2d56e.

--- shudai/SHViewController.h
+++ shudai/SHViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface SHViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
+@interface SHViewController : UIViewController <UITableViewDataSource, UITableViewDelegate, UIAlertViewDelegate>
 {
     //Variables
     IBOutlet UITableView *tablaSugerencias;
@@ -19,6 +19,7 @@
 @property (nonatomic, retain) UITableView *tablaSugerencias;
 @property (strong, nonatomic) IBOutlet UITextField *txtBuscar;
 @property (strong,nonatomic) NSString *txtClicado;
+@property (strong,nonatomic) NSString *campoTxt;
 - (IBAction)btnBuscar:(id)sender;
 - (IBAction)cerrarTeclado:(id)sender;
 
