9fda772fa779b61704e221319a00fdb99f3a9a81
David Guijarro <davidguijarro@Mac-mini-de-Worten.local>
2014-01-16 22:14:37 +0100

    Revert 3d6be63..260d918
    This rolls back to commit 3d6be63da875407641688773a1cd4523c644dff7.

--- shudai/SHViewController.h
+++ shudai/SHViewController.h
@@ -2,26 +2,12 @@
 //  SHViewController.h
 //  shudai
 //
-//  Created by Miriam Castro on 01/12/13.
+//  Created by David Guijarro on 26/11/13.
 //  Copyright (c) 2013 Studio Apparte. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface SHViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
-{
-    //Variables
-    IBOutlet UITableView *tablaSugerencias;
-    //NSString * txtClicado;
-}
-
-@property (readonly,nonatomic) NSMutableArray *listaSugerencias;
-@property (nonatomic, retain) UITableView *tablaSugerencias;
-@property (strong, nonatomic) IBOutlet UITextField *txtBuscar;
-@property (strong,nonatomic) NSString *txtClicado;
-- (IBAction)btnBuscar:(id)sender;
-- (IBAction)cerrarTeclado:(id)sender;
-
-
+@interface SHViewController : UIViewController
 
 @end
--- shudai/SHViewController.m
+++ shudai/SHViewController.m
@@ -2,14 +2,11 @@
 //  SHViewController.m
 //  shudai
 //
-//  Created by Miriam Castro on 01/12/13.
+//  Created by David Guijarro on 26/11/13.
 //  Copyright (c) 2013 Studio Apparte. All rights reserved.
 //
 
 #import "SHViewController.h"
-#import "Sugerencia.h"
-#import "SH2ViewController.h"
-
 
 @interface SHViewController ()
 
@@ -17,139 +14,16 @@
 
 @implementation SHViewController
 
-@synthesize listaSugerencias, tablaSugerencias, txtBuscar, txtClicado;
-
-
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
-    
-    
-    listaSugerencias = [[NSMutableArray alloc] init];
-    
-    //for (int i=0; i < 5; i++) {
-        
-       // NSString *sugTemporal = [[NSString alloc]initWithFormat:@"Sugerencia %d", i ];
-        
-        Sugerencia *miSugerencia = [[Sugerencia alloc] initWithNombre:@"Rajoy"
-                                                    stringComentarios:NULL];
-        Sugerencia *sugerenciaDOS =[[Sugerencia alloc] initWithNombre:@"Madrid"
-                                                    stringComentarios:NULL];
-        Sugerencia *sugerenciaTres = [[Sugerencia alloc] initWithNombre:@"Agosto"
-                                                      stringComentarios:NULL];
-        Sugerencia *sugerenciaCuatro = [[Sugerencia alloc] initWithNombre:@"Depeche_Mode"
-                                                    stringComentarios:NULL];
-        Sugerencia *sugerenciaCinco = [[Sugerencia alloc] initWithNombre:@"Rolling_Stones"
-                                                    stringComentarios:NULL];
-        Sugerencia *sugerenciaSeis = [[Sugerencia alloc] initWithNombre:@"LG"
-                                                    stringComentarios:NULL];
-        Sugerencia *sugerenciaSiete = [[Sugerencia alloc] initWithNombre:@"Tomates"
-                                                    stringComentarios:NULL];
-    
-        [listaSugerencias addObject:miSugerencia];
-        [listaSugerencias addObject:sugerenciaDOS];
-        [listaSugerencias addObject:sugerenciaTres];
-        [listaSugerencias addObject:sugerenciaCuatro];
-        [listaSugerencias addObject:sugerenciaCinco];
-        [listaSugerencias addObject:sugerenciaSeis];
-        [listaSugerencias addObject:sugerenciaSiete];
-    
-        
-    //}
+	// Do any additional setup after loading the view, typically from a nib.
 }
 
-
-- (IBAction)btnBuscar:(id)sender {
-    
-    //NSString *campoTexto = [txtBuscar text];
-    //NSLog(@"%@", campoTexto);
-    
-    //[self performSegueWithIdentifier:@"segundaVista" sender:campoTexto];
-    
-    
-    
-    
-}
-
-- (IBAction)cerrarTeclado:(id)sender {
-    
-    [sender resignFirstResponder]; //cerrar teclado
-}
-
-
-
-
-
-
-#pragma mark - Table view data source
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+- (void)didReceiveMemoryWarning
 {
-    
-    // Return the number of sections.
-    return 1;
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
 }
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
-{
-    
-    // Return the number of rows in the section.
-    return [listaSugerencias count];
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    Sugerencia *sugerenciaActual = [ listaSugerencias objectAtIndex:indexPath.row];
-    
-    static NSString *CellIdentifier = @"Cell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
-    
-    cell.textLabel.text = [sugerenciaActual nombreSugerencia];
-    
-    return cell;
-}
-
--(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
-    
-    
-    Sugerencia *sugerenciaClicada = [listaSugerencias objectAtIndex:indexPath.row];
-    
-    //txtBuscar.text = [sugerenciaClicada nombreSugerencia];
-    
-    
-    txtClicado = [sugerenciaClicada nombreSugerencia];
-    
-   
-    
-    [self performSegueWithIdentifier:@"segundaVista" sender:self.txtClicado];
-    
-
-}
-
-
-
-
-
-#pragma mark - Navigation
- 
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    if ([segue.identifier isEqualToString:@"segundaVista"])
-    {
-        NSString *texto = txtBuscar.text;
-        SH2ViewController *viewController = segue.destinationViewController;
-        viewController.campo2Texto = texto;
-        viewController.campo2Texto = self.txtClicado;
-//        NSLog(@"Funciona!");
-//        NSLog(@"%@", self.txtClicado);
-        
-    }
- 
- 
-}
- 
-
-
 
 @end
